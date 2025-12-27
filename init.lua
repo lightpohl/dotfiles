46929c4fc3ac9214db8f20bd2726841971434353
Josh Pohl <joshuapohl@gmail.com>
2025-12-26 19:53:27 -0700

    Add init.lua

---
+++ init.lua
@@ -0,0 +1,59 @@
+-- 1. Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_err_writeln("Failed to clone lazy.nvim:\n" .. out)
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 2. General Settings
+vim.g.mapleader = ","
+
+local opt = vim.opt
+opt.ignorecase = true
+opt.smartcase = true
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.termguicolors = true 
+opt.scrolloff = 8
+
+-- 3. Keymaps
+local map = vim.keymap.set
+map("n", "<leader>p", "<cmd>Telescope find_files<cr>")
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+map("n", "<leader>h", "<cmd>nohlsearch<cr>", { silent = true })
+
+-- 4. Plugins
+require("lazy").setup({
+  spec = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+      'nvim-telescope/telescope.nvim', 
+      branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function() 
+        -- THE NEW WAY: Direct setup on the main module
+        require("nvim-treesitter").setup({
+          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "python" },
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+      end
+    },
+  },
+  install = { colorscheme = { "catppuccin" } },
+})
+
+-- 5. Apply Colorscheme
+vim.cmd.colorscheme "catppuccin"
